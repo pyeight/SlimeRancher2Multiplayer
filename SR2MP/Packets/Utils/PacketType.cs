81667d3edb708e27c75beee120cd933a252127cf
py8 <74925512+pyeight@users.noreply.github.com>
2025-12-13 23:21:56 +0100

    Updated Packet Types

--- SR2MP/Packets/Utils/PacketType.cs
+++ SR2MP/Packets/Utils/PacketType.cs
@@ -1,18 +1,18 @@
 namespace SR2MP.Packets.Utils;
 
 public enum PacketType : byte
-{   // Type                       // Hierachy                                     // Exception                          // Use Case
+{   // Type                       // Hierarchy                                    // Exception                          // Use Case
     Connect = 0,                  // Client -> Server                                                                   Try to connect to Server
     ConnectAck = 1,               // Server -> Client                                                                   Initiate Player Join
     Close = 2,                    // Server -> All Clients                                                              Broadcast Server Close
-    PlayerJoin = 3,               // Server -> All Clients                        (except client that joins)            Add Player
-    // BroadcastPlayerJoin
-    PlayerLeave = 4,              // Client -> Server                                                                   Remove Player
-    BroadcastPlayerLeave = 5,     // Server -> All Clients                        (except client that left)             Remove Player for all clients
-    PlayerUpdate = 6,             // Client -> Server -> All Clients              (except updater)                      Update Player
-    // BroadcastPlayerUpdate
-    Heartbeat = 8,                // Client -> Server                                                                   Check if Clients are alive
-    HeartbeatAck = 9,             // Server -> Client                                                                   Automatically time the Clients out if the Server crashes
-    ChatMessage = 10,
-    BroadcastChatMessage = 11,
+    PlayerJoin = 3,               // Client -> Server                                                                   Add Player
+    BroadcastPlayerJoin = 4,      // Server -> All Clients                        (except client that joins)            Add Player on other Clients
+    PlayerLeave = 5,              // Client -> Server                                                                   Remove Player
+    BroadcastPlayerLeave = 6,     // Server -> All Clients                        (except client that left)             Remove Player on other Clients
+    PlayerUpdate = 7,             // Client -> Server                                                                   Update Player
+    BroadcastPlayerUpdate = 8,    // Server -> All Clients                        (except client that updates)          Update Player on other Clients
+    ChatMessage = 9,              // Client -> Server                                                                   Chat message
+    BroadcastChatMessage = 10,    // Server -> All clients                                                              Chat message on other Clients
+    Heartbeat = 16,               // Client -> Server                                                                   Check if Clients are alive
+    HeartbeatAck = 17,            // Server -> Client                                                                   Automatically time the Clients out if the Server crashes
 }
