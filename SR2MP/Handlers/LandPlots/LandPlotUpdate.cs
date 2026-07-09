aaa3cabe08c6d63d4828af7695140a31df5676a4
py8 <74925512+pyeight@users.noreply.github.com>
2026-07-09 16:48:47 +0200

    upgrade desync fix

--- SR2MP/Handlers/LandPlots/LandPlotUpdate.cs
+++ SR2MP/Handlers/LandPlots/LandPlotUpdate.cs
@@ -14,7 +14,8 @@ internal sealed class LandPlotUpgradeHandler : LandPlotUpdateHandler<LandPlotUpg
 {
     protected override bool Handle(LandPlotUpgradePacket packet, IPEndPoint? _)
     {
-        var model = GameState.landPlots[packet.PlotID];
+        if (!GameState.landPlots.TryGetValue(packet.PlotID, out var model))
+            return true;
 
         model.upgrades.Add(packet.ID);
 
@@ -41,7 +42,8 @@ internal sealed class NewLandPlotHandler : BasePacketHandler<NewLandPlotPacket>
 {
     protected override bool Handle(NewLandPlotPacket packet, IPEndPoint? _)
     {
-        var model = GameState.landPlots[packet.PlotID];
+        if (!GameState.landPlots.TryGetValue(packet.PlotID, out var model))
+            return true;
 
         model.typeId = packet.ID;
 
