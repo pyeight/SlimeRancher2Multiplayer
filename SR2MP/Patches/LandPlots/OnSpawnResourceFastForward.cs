75b4c412398f2b67b1d2537d851ecfdddda7057e
py8 <74925512+pyeight@users.noreply.github.com>
2026-07-24 12:34:59 +0200

    Fix chicken dupe & garden grow flickering

---
+++ SR2MP/Patches/LandPlots/OnSpawnResourceFastForward.cs
@@ -0,0 +1,18 @@
+using HarmonyLib;
+using SR2MP.Components.LandPlots;
+
+namespace SR2MP.Patches.LandPlots;
+
+[HarmonyPatch(typeof(SpawnResource), nameof(SpawnResource.FastForward))]
+internal static class OnSpawnResourceFastForward
+{
+    public static bool Prefix(SpawnResource __instance)
+    {
+        if (!Main.Server.IsRunning && !Main.Client.IsConnected) return true;
+
+        __instance.TryGetComponent<NetworkGarden>(out var networkGarden);
+        if (networkGarden == null) return true;
+
+        return networkGarden.LocallyOwned;
+    }
+}
